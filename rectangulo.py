6fa73d9f4d9769cddc0fbc03b357a5860f6caac7
Daniel Serrano Martin <danielserranoreal@gmail.com>
2025-03-28 08:43:39 +0000

    rectangulo

---
+++ rectangulo.py
@@ -0,0 +1,25 @@
+# rectangulo.py
+import math
+from punto import Punto
+
+class Rectangulo:
+    def __init__(self, inicial=None, final=None):
+        self.inicial = inicial if inicial is not None else Punto(0, 0)
+        self.final = final if final is not None else Punto(0, 0)
+    
+    def distancia(self):
+        dx = self.final.x - self.inicial.x
+        dy = self.final.y - self.inicial.y
+        return math.sqrt(dx**2 + dy**2)
+    
+    def base(self):
+        return abs(self.final.x - self.inicial.x)
+    
+    def altura(self):
+        return abs(self.final.y - self.inicial.y)
+    
+    def area(self):
+        return self.base() * self.altura()
+    
+    def __str__(self):
+        return f"Rectángulo con diagonal desde {self.inicial} hasta {self.final}"
